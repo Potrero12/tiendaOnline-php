5215a15fc1daeb319182982d24d55746d3b54213
Julian <julian.12040@gmail.com>
2022-07-10 09:23:50 -0500

    Final

--- controllers/CarritoController.php
+++ controllers/CarritoController.php
@@ -6,8 +6,12 @@
 
         public function index() {
             
-            $carrito = $_SESSION['carrito'];
-
+            if(isset($_SESSION['carrito']) && count($_SESSION['carrito']) >= 1){
+                $carrito = $_SESSION['carrito'];
+            } else {
+                $carrito = [];
+            }
+            
             require_once 'views/carrito/index.php';
         }
 
@@ -53,9 +57,39 @@
 
         }
 
-        public function remove(){
+        public function delete(){
 
-            
+            if(isset($_GET['index'])) {
+                $index = $_GET['index'];
+                unset($_SESSION['carrito'][$index]);
+            }
+
+            header('location:'.base_url."carrito/index");
+
+        }
+
+        public function up(){
+
+            if(isset($_GET['index'])) {
+                $index = $_GET['index'];
+                $_SESSION['carrito'][$index]['unidades']++;
+            }
+
+            header('location:'.base_url."carrito/index");
+
+        }
+
+        public function down(){
+
+            if(isset($_GET['index'])) {
+                $index = $_GET['index'];
+                $_SESSION['carrito'][$index]['unidades']--;
+                if($_SESSION['carrito'][$index]['unidades'] == 0) {
+                    unset($_SESSION['carrito'][$index]);
+                }
+            }
+
+            header('location:'.base_url."carrito/index");
 
         }
 
--- views/carrito/index.php
+++ views/carrito/index.php
@@ -1,41 +1,57 @@
 <h1>Carrito de la compra</h1>
 
-<table>
-    <tr>
-        <th>Imagen</th>
-        <th>Nombre</th>
-        <th>Precio</th>
-        <th>Unidades</th>
-    </tr>
-    <?php foreach($carrito as $index => $elemento) :
-            $producto = $elemento['producto'];
-            
-    ?>
+<?php if(isset($_SESSION['carrito']) && count($_SESSION['carrito']) >= 1) : ?>
+    <table>
         <tr>
-            <td>
-                <?php if($producto->imagen != null): ?>
-                    <img src="<?=base_url?>uploads/images/<?=$producto->imagen?>" class="img_carrito" alt="camiseta" />
-                <?php else:?>
-                    <img src="<?=base_url?>assets/img/camiseta.png" alt="default" class="img_carrito"/>
-                <?php endif;?>
-            </td>
-            <td>
-                <a href="<?=base_url?>producto/ver&id=<?=$producto->id?>"><?=$producto->nombre?></a>
-            </td>
-            <td>
-                <?=$producto->precio?>
-            </td>
-            <td>
-                <?=$elemento['unidades']?>
-            </td>
+            <th>Imagen</th>
+            <th>Nombre</th>
+            <th>Precio</th>
+            <th>Unidades</th>
+            <th>Accion</th>
         </tr>
+        <?php foreach($carrito as $index => $elemento) :
+                $producto = $elemento['producto'];
+                
+        ?>
+            <tr>
+                <td>
+                    <?php if($producto->imagen != null): ?>
+                        <img src="<?=base_url?>uploads/images/<?=$producto->imagen?>" class="img_carrito" alt="camiseta" />
+                    <?php else:?>
+                        <img src="<?=base_url?>assets/img/camiseta.png" alt="default" class="img_carrito"/>
+                    <?php endif;?>
+                </td>
+                <td>
+                    <a href="<?=base_url?>producto/ver&id=<?=$producto->id?>"><?=$producto->nombre?></a>
+                </td>
+                <td>
+                    <?=$producto->precio?>
+                </td>
+                <td>
+                    <?=$elemento['unidades']?>
+                    <div class="updown-unidades">
+                        <a href="<?=base_url?>carrito/up&index=<?=$index?>" class="button">+</a>
+                        <a href="<?=base_url?>carrito/down&index=<?=$index?>" class="button">-</a>
+                    </div>
+                </td>
+                <td>
+                <a href="<?=base_url?>carrito/delete&index=<?=$index?>" class="button button-carrito button-red">Quitar</a>
+                </td>
+            </tr>
 
-    <?php endforeach;?>
-</table>
+        <?php endforeach;?>
+    </table>
 
-<br />
-<div class="total-carrito">
-    <?php $stats = Utils::statsCarrito()?>
-    <h3>Precio Total: <?=$stats['total']?> €</h3>
-    <a href="<?=base_url?>pedido/hacer" class="button button-pedido">Hacer Pedido</a>
-</div>
+    <br />
+
+    <div class="delete-carrito">
+        <a href="<?=base_url?>carrito/deleteAll" class="button button-delete button-red">Vaciar Carrito</a>
+    </div>
+    <div class="total-carrito">
+        <?php $stats = Utils::statsCarrito()?>
+        <h3>Precio Total: <?=$stats['total']?> €</h3>
+        <a href="<?=base_url?>pedido/hacer" class="button button-pedido">Hacer Pedido</a>
+    </div>
+<?php else: ?>
+    <p>El carrito esta vacio, añade productos</p>
+<?php endif; ?>
